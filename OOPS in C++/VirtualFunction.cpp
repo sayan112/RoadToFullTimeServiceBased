29304ab2414eac3215260988af80ae65575d6b60
SAYAN MAITRA <75684139+sayan112@users.noreply.github.com>
2023-11-10 16:58:50 +0530

    "all fixed"

--- OOPS in C++/VirtualFunction.cpp
+++ OOPS in C++/VirtualFunction.cpp
@@ -1,31 +1,8 @@
-#include <iostream>
-
-class Base {
-public:
-    virtual void virtualFunction() {
-        std::cout << "Base class virtual function" << std::endl;
-    }
-};
-
-class Derived : public Base {
-public:
-    void virtualFunction() override {
-        std::cout << "Derived class virtual function" << std::endl;
-    }
-};
-
-int main() {
-    Base* basePtr = new Derived(); // Pointer to a derived class object
-    basePtr->virtualFunction();    // Calls the derived class function
-
-    delete basePtr;
-    return 0;
-}
 
 /*
   virtual is a keyword in c++
 In C++, a virtual function is a member function of a base class that can be overridden by a derived class.It allows dynamic binding or late binding, which means that the appropriate function to call is determined at runtime based on the type of the object being referred to.
-
+When a function is declared as virtual in a base class, it indicates that this function can be overridden in derived classes. 
 */
 
 #include <bits./stdc++.h>
@@ -33,7 +10,7 @@ using namespace std;
 class Animal
 {
 public:
-   virtual void color()
+  virtual void color()
     {
         cout << "grey" << endl;
     }
@@ -48,17 +25,11 @@ public:
 };
 int main()
 {
-    // Dog germanshepherd;
-    // germanshepherd.color();
-    Animal *baseptr;
-
-//     Dog germanShephard;
-//     baseptr= &germanShephard;
-//  baseptr->color();
-
-        Animal germanShephard;
-        baseptr= &germanShephard;
-     baseptr->color();
+    Dog germanshepherd;
+    germanshepherd.color(); // Calls the overridden function in Dog class (black)
+    Animal beagal;
+    beagal.color(); // Calls the virtual function in Animal class (grey)
+    // you create an object of type `Dog` and an object of type `Animal`.When you call the `color()` function on the `germanshepherd` object, it calls the overridden function in the `Dog` class, printing "black." When you call the `color()` function on the `beagal` object, it calls the virtual function from the `Animal` class, printing "grey."
 }
 
 
@@ -66,16 +37,17 @@ int main()
 
 
 
-// A pure virtual function is a virtual function in C++ for which we need not write any function definition and only have to declare it. It is declared by assigning 0 in the declaration.
-// Syntax:
 
-/*
-  
-  classA
-{
- public:
-     virtualvoids() = 0; // Pure Virtual Function };
+    // A pure virtual function is a virtual function in C++ for which we need not write any function definition and only have to declare it. It is declared by assigning 0 in the declaration.
+    // Syntax:
 
-     */
+    /*
 
-//  A pure virtual function (or abstract function) in C++ is a virtual function for which we can implement, But we must override that function in the derived class; otherwise, the derived class will also become an abstract class.
+      classA
+    {
+     public:
+         virtualvoids() = 0; // Pure Virtual Function };
+
+         */
+
+    //  A pure virtual function (or abstract function) in C++ is a virtual function for which we can implement, But we must override that function in the derived class; otherwise, the derived class will also become an abstract class.
---
+++ OOPS in C++/staticKeyword&staticFunctions.md
@@ -0,0 +1,62 @@
+In C++, the `static` keyword is used in different contexts, and its meaning can vary depending on where it's used. Let's discuss the `static` keyword in the context of functions.
+
+### 1. Static Member Functions:
+
+In the context of member functions in a class, the `static` keyword is used to declare a function as a static member function. A static member function is a member function that belongs to the class rather than instances of the class. It can be called on the class itself, without creating an instance of the class. It does not have access to the `this` pointer, and it cannot access non-static members of the class.
+
+```cpp
+class MyClass {
+public:
+    // Static member function
+    static void staticFunction() {
+        // Code here
+    }
+};
+
+// Call the static function without creating an object
+MyClass::staticFunction();
+```
+
+### 2. Static Local Variables:
+
+In the context of local variables within a function, the `static` keyword is used to declare a variable as static. A static local variable retains its value between function calls. It is initialized only once, and its value is preserved between function calls.
+
+```cpp
+void myFunction() {
+    // Static local variable
+    static int counter = 0;
+
+    // Access and modify the static variable
+    counter++;
+
+    // Rest of the function code
+}
+```
+
+In the example above, the `counter` variable will be initialized only once, and its value will be retained between calls to `myFunction()`.
+
+### 3. Static Global Variables:
+
+Outside of functions and classes, the `static` keyword can be used to declare a global variable with internal linkage. This means that the variable is only visible within the translation unit (source file) where it is declared, and it is not visible to other translation units linked into the program.
+
+```cpp
+// Static global variable with internal linkage
+static int globalVariable = 42;
+```
+
+### 4. Static Member Variables:
+
+In the context of class members, the `static` keyword can also be used to declare static member variables. These variables are shared among all instances of the class, and they exist independently of any particular instance of the class.
+
+```cpp
+class MyClass {
+public:
+    // Static member variable
+    static int staticVariable;
+};
+
+// Initialization of static member variable
+int MyClass::staticVariable = 0;
+```
+
+These are the primary uses of the `static` keyword in the context of functions and variables in C++.
